9381960849586bdb66844d593bbdd4d7a05fda87
Josh West <joshw@zillowgroup.com>
2018-06-07 14:43:28 -0700

    style and functionality

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+// reverse a string
+function reverseString() {
+
+}
+
+
+const output = reverseString('hello');
+console.log(output);
+
+
+function myAnswer() {
+  let answer = document.getElementById('show').innerHTML = "test works!";
+
+};
