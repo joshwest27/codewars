158e223961bb2110e6d9b8526e1e24018c55856a
Josh West <joshw@zillowgroup.com>
2018-06-08 09:59:40 -0700

    more variations of reverse string

--- js/app.js
+++ js/app.js
@@ -2,34 +2,34 @@
 'use strict';
 
 // reverse a string
-// function reverseString(str) {
-//   let strArr = str.split('');
-//   strArr.reverse();
-//   return strArr.join('');
-// }
+function reverseString(str) {
+  let strArr = str.split('');
+  strArr.reverse();
+  return strArr.join('');
+}
 
 // cleaner code from above
-// function reverseString(str) {
-//   return str.split('').reverse('').join('');
-// }
+function reverseString(str) {
+  return str.split('').reverse('').join('');
+}
 
 // for loop variation decreasing
-// function reverseString(str) {
-//   let revString = '';
-//   for(let i = str.length - 1; i >= 0; i--) {
-//     revString = revString + str[i];
-//   }
-//   return revString;
-// }
+function reverseString(str) {
+  let revString = '';
+  for(let i = str.length - 1; i >= 0; i--) {
+    revString = revString + str[i];
+  }
+  return revString;
+}
 
 // for loop variation increasing
-// function reverseString(str) {
-//   let revString = '';
-//   for(let i in str) {
-//     revString = str[i] + revString;
-//   }
-//   return revString;
-// }
+function reverseString(str) {
+  let revString = '';
+  for(let i in str) {
+    revString = str[i] + revString;
+  }
+  return revString;
+}
 
 // for of loop
 function reverseString(str) {
@@ -37,6 +37,14 @@ function reverseString(str) {
   for(let char of str) {
     revString = char + revString;
   }
+  return revString;
+}
+
+function reverseString(str) {
+  let revString = '';
+  str.split('').forEach(function(char) {
+    revString = char + revString;
+  });
   return revString;
 }
 
